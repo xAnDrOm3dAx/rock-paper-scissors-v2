156d8cc4452629f1b31b08f2b960a48a6cc6045d
xAnDrOm3dAx <123436349+xAnDrOm3dAx@users.noreply.github.com>
2023-07-05 13:31:18 +1000

    remove console.log

--- index.js
+++ index.js
@@ -16,7 +16,6 @@ function getComputerChoice () {
     return computerChoice
 }
 
-console.log(getComputerChoice())
  // Write a function that plays a single round of Rock Paper Scissors. The function should take two parameters - the playerSelection and computerSelection - and then return a string that declares the winner of the round like so: "You Lose! Paper beats Rock"
  
 
