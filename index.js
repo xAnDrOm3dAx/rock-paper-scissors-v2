1e63d593e91461ebf809fa8a80527cab95d6b61e
xAnDrOm3dAx <123436349+xAnDrOm3dAx@users.noreply.github.com>
2023-07-05 16:23:29 +1000

    update function getComputerChoice()

--- index.js
+++ index.js
@@ -2,29 +2,18 @@
 //--------------------Step One--------------------------
 // Randomly generate Rock, Paper or Scissors
 
-function getComputerChoice () {
-    const weapons = ["rock", "paper", "scissors"];
-    const randomIndex = Math.floor(Math.random() * weapons.length)
-    const computerChoice = weapons[randomIndex]
-    return computerChoice
+function getComputerChoice() {
+    const weapons = ["Rock", "Paper", "Scissors"];
+    const randomWeapon = weapons[Math.floor(Math.random() * weapons.length)]
+    return randomWeapon;
 }
 
-console.log(getComputerChoice())
+console.log(getComputerChoice());
 
 
 //---------------------Step Two-------------------------
 // Play a single round of the game (this is the game logic)
 
-
-function playRound(playerSelection, computerSelection) {
-        if (playerSelection === computerSelection) {
-            return "We have a tie!"
-        } 
-    } 
-   
-  const playerSelection = "rock";
-  const computerSelection = getComputerChoice();
-  console.log(playRound(playerSelection, computerSelection));
 
 
 //--------------------Final Part------------------------
