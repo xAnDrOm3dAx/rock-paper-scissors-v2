d9b72f0a4c545e8bd24450e939dfaacf5c53d022
xAnDrOm3dAx <123436349+xAnDrOm3dAx@users.noreply.github.com>
2023-07-06 11:05:52 +1000

    add function playRound

--- index.js
+++ index.js
@@ -7,15 +7,51 @@ function getComputerChoice() {
     return randomWeapon;
 }
 
-console.log(getComputerChoice());
-
 // Initialize scores at 0
 
 let playerScore = 0
 let computerScore = 0 
 
+
 // Play a single round of the game
 
+// Version 1
 
-//--------------------Final Part------------------------
-// Play the game 5 times for a round
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return `Both players chose ${playerSelection}, it's a tie!`;
+    } else if (playerSelection === "rock" && computerSelection === "scissors") {
+        return "Rock obliterates scissors, Player wins!";
+    } else if (playerSelection === "scissors" && computerSelection === "rock") {
+        return "Rock obliterates scissors, Computer wins!!";
+    } else if (playerSelection === "scissors" && computerSelection === "paper") {
+        return "Scissors shreds paper, Player wins!";
+    } else if (playerSelection === "paper" && computerSelection === "scissors") {
+        return "Scissors shreds paper, Computer wins!";
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
+        return "Paper smothers rock, Player wins!";
+    } else if (playerSelection === "rock" && computerSelection === "paper") {
+        return "Paper smothers rock, Computer wins!";
+    } 
+}
+
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log("Computer:" + " " + computerSelection);
+console.log("Player:" + " " + playerSelection);
+console.log(playRound(playerSelection, computerSelection));
+
+
+
+// //--------------------Final Part------------------------
+// // Play the game 5 times for a round
+
+
+
+
+
+
+
+// console.log("Computer Score" + " " + computerScore);
+// console.log("Player Score" + " " + playerScore);
