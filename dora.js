e490bf97ac68a7e228ed5e4d9486190104c43e69
xAnDrOm3dAx <123436349+xAnDrOm3dAx@users.noreply.github.com>
2023-07-07 15:01:38 +1000

    create functions checkWinner() & playRound()

--- dora.js
+++ dora.js
@@ -1,13 +1,64 @@
-
-// Create a variable that holds the values of ‘Rock’, ‘Paper’ or ‘Scissors’ within an array.
+// 1. Start a new Git Repo.
+// 2. Create a new HTML document and like the JS file.
+// 3. Create a variable that holds the values of ‘Rock’, ‘Paper’ or ‘Scissors’ within an array.
 
 const options = ["rock", "paper", "scissors"];
 
-//  Create function called getComputerChoice that will randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’. 
+
+// 3. Create function called getComputerChoice that will randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’. 
 
 function getComputerChoice() {
     const randomIndex = options[Math.floor(Math.random() * options.length)];
     return randomIndex;
 }
 
-getComputerChoice()
+
+// 4.1 Check the winner based on the choices
+
+function checkWinner(playerSelection, computerSelection) {
+
+    if (playerSelection === computerSelection) {
+        return "Tie";
+    } else if ((playerSelection === "rock" && computerSelection === "scissors") ||
+        (playerSelection === "scissors" && computerSelection === "paper") ||
+        (playerSelection === "paper" && computerSelection === "scissors")) {
+        return "Player Wins"
+        } else {
+            return "Computer Wins"
+        }
+}
+
+
+// 4. Write a function that plays a single round of rock, paper scissors
+
+function playRound(playerSelection, computerSelection) {
+    const result = (checkWinner(playerSelection, computerSelection))
+    if (result === `Tie`) {
+        return `Both players chose ${playerSelection}, it's a tie!`
+    } else if (result === "Player Wins") {
+        return `You win! ${playerSelection} beats ${computerSelection}`
+    } else if (result === "Computer Wins") {
+        return `You lose! ${computerSelection} beats ${playerSelection}`
+    }
+    
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log("Player chose:" + " " + playerSelection);
+console.log("Computer chose:" + " " + computerSelection);
+console.log(playRound(playerSelection, computerSelection));
+
+
+
+    
+
+        
+                
+
+    
+
+
+
+
+getComputerChoice();
