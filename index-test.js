3b6940b306b612a0d2ab7307197e29c9f58ea02f
xAnDrOm3dAx <123436349+xAnDrOm3dAx@users.noreply.github.com>
2023-07-17 17:34:04 +1000

    complete function playRound()

--- index-test.js
+++ index-test.js
@@ -15,7 +15,23 @@ function getPlayerChoice() {}
 
 // Play a single round of the game and define the possible outcomes
 
-function playRound(playerSelection, computerSelection) {}
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection === computerSelection) {
+    return `Both players chose ${playerSelection}, it's a tie!`;
+  } else if (playerSelection === "rock" && computerSelection === "scissors") {
+    return "Rock obliterates scissors, Player wins!";
+  } else if (playerSelection === "scissors" && computerSelection === "rock") {
+    return "Rock obliterates scissors, Computer wins!";
+  } else if (playerSelection === "scissors" && computerSelection === "paper") {
+    return "Scissors shreds paper, Player wins!";
+  } else if (playerSelection === "paper" && computerSelection === "scissors") {
+    return "Scissors shreds paper, Computer wins!";
+  } else if (playerSelection === "paper" && computerSelection === "rock") {
+    return "Paper smothers rock, Player wins!";
+  } else if (playerSelection === "rock" && computerSelection === "paper") {
+    return "Paper smothers rock, Computer wins!";
+  }
+}
 
 // Create a function called game, the previous function (playRound()) inside of this one to play a 5 round game that keeps score and reports a winner or loser at the end.
 
