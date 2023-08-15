b544b88e018b268fca8cac3146d1663c18fda433
xAnDrOm3dAx <123436349+xAnDrOm3dAx@users.noreply.github.com>
2023-08-15 11:47:10 +1000

    disable 5 rounds, initialise scores
    disable logic which plays 5 rounds and move initialise scores to top of code

--- script.js
+++ script.js
@@ -2,6 +2,12 @@
 
 const choices = ["rock", "paper", "scissors"];
 
+// Initialize scores at 0 and increment when playing
+
+let playerScore = 0;
+let computerScore = 0;
+console.log("Welcome! Please make your selection. Best of 5 rounds wins the game.");
+
 // Create function called getComputerChoice that will randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’.
 
 function getComputerChoice() {
@@ -48,45 +54,39 @@ function playRound(playerSelection, computerSelection) {
 // Create a function called game, the previous function (playRound()) inside of this one to play a 5 round game that keeps score and reports a winner or loser at the end.
 
 function game() {
-  // Initialize scores at 0 and increment when playing
-
-  let playerScore = 0;
-  let computerScore = 0;
-  console.log("Welcome! Please make your selection. Best of 5 rounds wins the game.");
-
   // Play the game 5 times per round, increment the winner score by one, log the choices and total score for player and computer on each round.
 
-  for (let round = 1; round <= 5; round++) {
-    const playerSelection = getPlayerChoice();
-    const computerSelection = getComputerChoice();
-    const result = playRound(playerSelection, computerSelection);
+  // for (let round = 1; round <= 5; round++) {
+  const playerSelection = getPlayerChoice();
+  const computerSelection = getComputerChoice();
+  const result = playRound(playerSelection, computerSelection);
 
-    if (result.includes("Player")) {
-      playerScore++;
-    } else if (result.includes("Computer")) {
-      computerScore++;
-    }
-
-    console.log("-----------------------------------");
-    console.log(`Round ${round}`);
-    console.log("Computer chose:" + " " + `${computerSelection}`);
-    console.log("Player chose:" + " " + `${playerSelection}`);
-    console.log(`${result}`);
-    console.log(`Player Score: ${playerScore}`);
-    console.log(`Computer Score: ${computerScore}`);
+  if (result.includes("Player")) {
+    playerScore++;
+  } else if (result.includes("Computer")) {
+    computerScore++;
   }
 
-  if (playerScore > computerScore) {
-    console.log("-----------------------------------");
-    console.log("Game Over! Player Wins!");
-  } else if (playerScore < computerScore) {
-    console.log("-----------------------------------");
-    console.log("Game Over, Computer Wins!");
-  } else {
-    console.log("-----------------------------------");
-    console.log("Game Over, it's a tie! Please try again.");
-  }
+  console.log("-----------------------------------");
+  console.log(`Round ${round}`);
+  console.log("Computer chose:" + " " + `${computerSelection}`);
+  console.log("Player chose:" + " " + `${playerSelection}`);
+  console.log(`${result}`);
+  console.log(`Player Score: ${playerScore}`);
+  console.log(`Computer Score: ${computerScore}`);
 }
+
+if (playerScore > computerScore) {
+  console.log("-----------------------------------");
+  console.log("Game Over! Player Wins!");
+} else if (playerScore < computerScore) {
+  console.log("-----------------------------------");
+  console.log("Game Over, Computer Wins!");
+} else {
+  console.log("-----------------------------------");
+  console.log("Game Over, it's a tie! Please try again.");
+}
+// }
 
 // Invoke game function
 
