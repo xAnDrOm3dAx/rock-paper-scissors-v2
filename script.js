02b366782aea5a690299528d2cf9c191f2f67fb8
Christopher Werkmeister <123436349+xAnDrOm3dAx@users.noreply.github.com>
2024-01-08 14:27:19 +1000

    swap weapon array for enum object for clarity. Add const variable for winning threshold.

--- script.js
+++ script.js
@@ -33,36 +33,34 @@ paperButton.addEventListener("click", () => {
   checkForWinner();
 });
 
-// Create a variable that holds the values of ‘Rock’, ‘Paper’ or ‘Scissors’ within an array.
-
-const choices = ["✊", "✋", "✌"];
-
-// Initialize scores at 0 and increment when playing
-
+// Initialize scores at 0
 let playerScore = 0;
 let computerScore = 0;
 
-// Create function called getComputerChoice that will randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’.
+// Create an Enum which holds three properties and thier values which represent rock, paper ands scissors.
+const Weapon = {
+  ROCK: "✊",
+  PAPER: "✋",
+  SCISSORS: "✌",
+};
 
+// Create function called getComputerChoice that will randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’.
 function getComputerChoice() {
-  const randomIndex = choices[Math.floor(Math.random() * choices.length)];
+  const weapons = [Weapon.ROCK, Weapon.PAPER, Weapon.SCISSORS];
+  const randomIndex = weapons[Math.floor(Math.random() * weapons.length)];
   return randomIndex;
 }
 
 // Play a single round of the game and define the possible outcomes
-const resultsWindow = document.querySelector("#round-results");
-const playerResult = document.querySelector("#player-result");
-const computerResult = document.querySelector("#computer-result");
-
 function playRound(playerSelection, computerSelection) {
   if (playerSelection === computerSelection) {
     gameOver.textContent = "It's a tie!";
     playerWeapon.textContent = playerSelection;
     computerWeapon.textContent = computerSelection;
   } else if (
-    (playerSelection === "✊" && computerSelection === "✌") ||
-    (playerSelection === "✌" && computerSelection === "✋") ||
-    (playerSelection === "✋" && computerSelection === "✊")
+    (playerSelection === Weapon.ROCK && computerSelection === Weapon.SCISSORS) ||
+    (playerSelection === Weapon.SCISSORS && computerSelection === Weapon.PAPER) ||
+    (playerSelection === Weapon.PAPER && computerSelection === Weapon.ROCK)
   ) {
     gameOver.textContent = "Player wins!";
     playerWeapon.textContent = playerSelection;
@@ -70,9 +68,9 @@ function playRound(playerSelection, computerSelection) {
     playerScore++;
     playerScoreDisplay.textContent = `Player Score = ${playerScore}`;
   } else if (
-    (playerSelection === "✌" && computerSelection === "✊") ||
-    (playerSelection === "✋" && computerSelection === "✌") ||
-    (playerSelection === "✊" && computerSelection === "✋")
+    (playerSelection === Weapon.SCISSORS && computerSelection === Weapon.ROCK) ||
+    (playerSelection === Weapon.PAPER && computerSelection === Weapon.SCISSORS) ||
+    (playerSelection === Weapon.ROCK && computerSelection === Weapon.PAPER)
   ) {
     gameOver.textContent = "Computer wins!";
     playerWeapon.textContent = playerSelection;
@@ -83,7 +81,8 @@ function playRound(playerSelection, computerSelection) {
 }
 
 function checkForWinner() {
-  if (playerScore === 5) {
+  const WINNING_THRESHOLD = 5;
+  if (playerScore === WINNING_THRESHOLD) {
     buttons.textContent = "";
     outcomeText.textContent = "Game Over...";
     gameOver.textContent = "Player has 5 points";
@@ -105,3 +104,111 @@ function resetGame() {
     window.location.reload(); // Reload the page when start button is clicked
   });
 }
+
+// // Grab all necessary elements
+
+// const rockButton = document.querySelector(".rock");
+// const paperButton = document.querySelector(".paper");
+// const scissorsButton = document.querySelector(".scissors");
+// const buttons = document.querySelector(".buttons");
+// const playerWeapon = document.querySelector(".player-weapon");
+// const computerWeapon = document.querySelector(".computer-weapon");
+// const outcomeText = document.querySelector(".win-or-lose");
+// const playerScoreDisplay = document.querySelector(".player-score");
+// const computerScoreDisplay = document.querySelector(".computer-score");
+// const gameOver = document.querySelector(".game-over");
+
+// //  Add event listeners for individual RPS buttons.
+// rockButton.addEventListener("click", () => {
+//   computerSelection = getComputerChoice();
+//   const playerSelection = "✊";
+//   playRound(playerSelection, computerSelection);
+//   checkForWinner();
+// });
+
+// scissorsButton.addEventListener("click", () => {
+//   computerSelection = getComputerChoice();
+//   const playerSelection = "✌";
+//   playRound(playerSelection, computerSelection);
+//   checkForWinner();
+// });
+
+// paperButton.addEventListener("click", () => {
+//   computerSelection = getComputerChoice();
+//   const playerSelection = "✋";
+//   playRound(playerSelection, computerSelection);
+//   checkForWinner();
+// });
+
+// // Create a variable that holds the values of ‘Rock’, ‘Paper’ or ‘Scissors’ within an array.
+
+// const choices = ["✊", "✋", "✌"];
+
+// // Initialize scores at 0 and increment when playing
+
+// let playerScore = 0;
+// let computerScore = 0;
+
+// // Create function called getComputerChoice that will randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’.
+
+// function getComputerChoice() {
+//   const randomIndex = choices[Math.floor(Math.random() * choices.length)];
+//   return randomIndex;
+// }
+
+// // Play a single round of the game and define the possible outcomes
+// const resultsWindow = document.querySelector("#round-results");
+// const playerResult = document.querySelector("#player-result");
+// const computerResult = document.querySelector("#computer-result");
+
+// function playRound(playerSelection, computerSelection) {
+//   if (playerSelection === computerSelection) {
+//     gameOver.textContent = "It's a tie!";
+//     playerWeapon.textContent = playerSelection;
+//     computerWeapon.textContent = computerSelection;
+//   } else if (
+//     (playerSelection === "✊" && computerSelection === "✌") ||
+//     (playerSelection === "✌" && computerSelection === "✋") ||
+//     (playerSelection === "✋" && computerSelection === "✊")
+//   ) {
+//     gameOver.textContent = "Player wins!";
+//     playerWeapon.textContent = playerSelection;
+//     computerWeapon.textContent = computerSelection;
+//     playerScore++;
+//     playerScoreDisplay.textContent = `Player Score = ${playerScore}`;
+//   } else if (
+//     (playerSelection === "✌" && computerSelection === "✊") ||
+//     (playerSelection === "✋" && computerSelection === "✌") ||
+//     (playerSelection === "✊" && computerSelection === "✋")
+//   ) {
+//     gameOver.textContent = "Computer wins!";
+//     playerWeapon.textContent = playerSelection;
+//     computerWeapon.textContent = computerSelection;
+//     computerScore++;
+//     computerScoreDisplay.textContent = `Computer Score = ${computerScore}`;
+//   }
+// }
+
+// function checkForWinner() {
+//   if (playerScore === 5) {
+//     buttons.textContent = "";
+//     outcomeText.textContent = "Game Over...";
+//     gameOver.textContent = "Player has 5 points";
+//     resetGame();
+//   } else if (computerScore === 5) {
+//     buttons.textContent = "";
+//     outcomeText.textContent = "Game Over...";
+//     gameOver.textContent = "Computer has 5 points";
+//     resetGame();
+//   }
+// }
+
+// function resetGame() {
+//   const startButton = document.createElement("button");
+//   startButton.classList.add("start-button");
+//   startButton.textContent = "START GAME";
+//   buttons.appendChild(startButton);
+//   startButton.addEventListener("click", () => {
+//     window.location.reload(); // Reload the page when start button is clicked
+//   });
+// }
