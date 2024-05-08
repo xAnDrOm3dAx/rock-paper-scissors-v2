d776fcbc7e906620f35f1bb1760d5bd2b9b4d165
xAnDrOm3dAx <123436349+xAnDrOm3dAx@users.noreply.github.com>
2024-05-09 08:56:47 +1000

    add animation to game result

--- script.js
+++ script.js
@@ -39,12 +39,14 @@ const Weapon = {
 function handlePlayerChoice(playerSelection) {
   playerWeapon.classList.add("weapon-animation");
   computerWeapon.classList.add("weapon-animation");
+  gameOver.classList.add("weapon-animation");
   computerSelection = getComputerChoice();
   playRound(playerSelection, computerSelection);
   setTimeout(() => {
     // Delay slightly to allow animation to play
     playerWeapon.classList.remove("weapon-animation");
     computerWeapon.classList.remove("weapon-animation");
+    gameOver.classList.remove("weapon-animation");
   }, 200); // Adjust the delay as needed based on animation duration
   checkForWinner();
 }
